33b008286b20c3fc40cc0392f0a7911302693480
mongi <mmby85@yahoo.fr>
2021-12-18 16:32:28 +0100

    first commit

--- alpha/scheduler/scheduler.py
+++ alpha/scheduler/scheduler.py
@@ -1,8 +1,9 @@
+from app.settings import TIME_ZONE
 from apscheduler.schedulers.background import BackgroundScheduler
 from alpha.views import DataViewset
 
 def start():
-  scheduler = BackgroundScheduler()
+  scheduler = BackgroundScheduler({'apscheduler.timezone': TIME_ZONE})
   data = DataViewset()
-  scheduler.add_job(data.save_data_api, "interval", minutes=60,id="data_001",replace_existing=True)
+  scheduler.add_job(data.save_data_api, "interval", minutes=1,id="data_001",replace_existing=True)
   scheduler.start()
